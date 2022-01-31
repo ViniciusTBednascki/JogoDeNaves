52e24bacd198f304bfc9d1689a591fea75e3e893
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2022-01-31 14:00:56 -0300

    criando a função de disparo

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ function start() {
 }
 	
 var jogo = {}
+var podeAtirar=true;
 var velocidade=5;
 var posicaoY = parseInt((Math.random() * 324) + 10);
 var TECLA = {
@@ -66,7 +67,7 @@ function movejogador() {
 	}
 	
 	if (jogo.pressionou[TECLA.D]) {
-    
+    disparo();
 	}
 
 }
@@ -108,4 +109,37 @@ function moveamigo() {
 					
 		}
 
+}
+
+function disparo() {
+	
+	if (podeAtirar==true) {
+		
+    podeAtirar=false;
+    
+    topo = parseInt($("#jogador").css("top"))
+    posicaoX= parseInt($("#jogador").css("left"))
+    tiroX = posicaoX + 190;
+    topoTiro=topo+37;
+    $("#fundoGame").append("<div id='disparo'></div");
+    $("#disparo").css("top",topoTiro);
+    $("#disparo").css("left",tiroX);
+    
+    var tempoDisparo=window.setInterval(executaDisparo, 30);
+    
+	}
+ 
+  function executaDisparo() {
+    posicaoX = parseInt($("#disparo").css("left"));
+    $("#disparo").css("left",posicaoX+15); 
+
+    if (posicaoX>900) {
+        
+      window.clearInterval(tempoDisparo);
+      tempoDisparo=null;
+      $("#disparo").remove();
+      podeAtirar=true;
+      
+    }
+	}
 }
